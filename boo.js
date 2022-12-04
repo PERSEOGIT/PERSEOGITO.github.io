d864dac50f3786ede0d208547bbdda5a700ca768
PERSEOGIT <119781925+PERSEOGIT@users.noreply.github.com>
2022-12-04 16:37:53 -0600

    Update boo.js

--- boo.js
+++ boo.js
@@ -1,5 +1,25 @@
-var texto = document.getElementById("textow");
-var nombre = prompt("escribe tu nombre");
-texto.innerHTML=nombre;
+var texto = document.getElementById("11");
+//var nombre = prompt("escribe tu nombre");
+//texto.innerHTML=nombre;
+var momento = 0;
+setInterval(()=>{
+	momento++;
+	if (momento == 5000){
+		texto.innerHTML="QUE"
+	}
+	if (momento == 7500){
+		texto.innerHTML="QUE ES"
+	}
+	if (momento == 9000){
+		texto.innerHTML="QUE ES UNA IA"
+	}
+
+})
+
+
+
+
+
+
 
 
